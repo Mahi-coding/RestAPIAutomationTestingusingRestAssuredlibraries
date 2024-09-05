462692f49348b8864171e5e36117a31d4248288b
Mahi <65525873+Mahi-coding@users.noreply.github.com>
2024-09-05 23:21:06 +0530

    hi

---
+++ src/ComplexJsonParse.java
@@ -0,0 +1,14 @@
+import files.payload;
+import io.restassured.path.json.JsonPath;
+
+public class ComplexJsonParse {
+
+	public static void main(String[] args) {
+		
+	JsonPath js = new JsonPath(payload.CoursePrice());
+		
+		
+
+	}
+
+}
--- src/files/payload.java
+++ src/files/payload.java
@@ -25,4 +25,54 @@ public class payload {
     		 + "";
 	}
 
+	public static String CoursePrice()
+	{
+		
+		return "{\r\n"
+				+ "\r\n"
+				+ "\"dashboard\": {\r\n"
+				+ "\r\n"
+				+ "\"purchaseAmount\": 910,\r\n"
+				+ "\r\n"
+				+ "\"website\": \"rahulshettyacademy.com\"\r\n"
+				+ "\r\n"
+				+ "},\r\n"
+				+ "\r\n"
+				+ "\"courses\": [\r\n"
+				+ "\r\n"
+				+ "{\r\n"
+				+ "\r\n"
+				+ "\"title\": \"Selenium Python\",\r\n"
+				+ "\r\n"
+				+ "\"price\": 50,\r\n"
+				+ "\r\n"
+				+ "\"copies\": 6\r\n"
+				+ "\r\n"
+				+ "},\r\n"
+				+ "\r\n"
+				+ "{\r\n"
+				+ "\r\n"
+				+ "\"title\": \"Cypress\",\r\n"
+				+ "\r\n"
+				+ "\"price\": 40,\r\n"
+				+ "\r\n"
+				+ "\"copies\": 4\r\n"
+				+ "\r\n"
+				+ "},\r\n"
+				+ "\r\n"
+				+ "{\r\n"
+				+ "\r\n"
+				+ "\"title\": \"RPA\",\r\n"
+				+ "\r\n"
+				+ "\"price\": 45,\r\n"
+				+ "\r\n"
+				+ "\"copies\": 10\r\n"
+				+ "\r\n"
+				+ "}\r\n"
+				+ "\r\n"
+				+ "]\r\n"
+				+ "\r\n"
+				+ "}";
+	
+}
 }
