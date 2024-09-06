2538cb290c1e296101fb11fd963ddf4e24950807
Mahi <65525873+Mahi-coding@users.noreply.github.com>
2024-09-06 15:15:44 +0530

    hi

--- src/ComplexJsonParse.java
+++ src/ComplexJsonParse.java
@@ -7,8 +7,34 @@ public class ComplexJsonParse {
 		
 	JsonPath js = new JsonPath(payload.CoursePrice());
 		
-		
+	//Print No ofcourses returned by API
+		int count = js.getInt("courses.size()");
+System.out.println(count);	
+
+//Print purchase amount
+
+int totalamount = js.getInt("dashboard.purchaseAmount");
+
+System.out.println(totalamount);
+
+//Print title of the first course
+
+String titlefirstcourse = js.get("courses[0].title");
+System.out.println(titlefirstcourse);
+
+//Print All course titles and their respective Prices
+
+for(int i=0;i<count;i++)
+{
+	
+  String courseTitles = js.get("courses["+i+"].title");
+  
+  
+  
+  System.out.println(courseTitles);
+  
+  System.out.println(js.get("courses["+i+"].price").toString());
 
 	}
-
+	}
 }
